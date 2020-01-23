8eff1d69172f5ec92b1a1f12755d1004a3388938
Antonio Garzon iniesta <agarzon-@c2r6s5.42madrid.com>
2020-01-23 18:32:03 +0100

    Get next line coment

--- cub3d.h
+++ cub3d.h
@@ -1,7 +1,7 @@
 #include "./minilibx_opengl_20191021/mlx.h"
 #include "./minilibx_mms_20191025_beta/mlx.h"
 #include "./libft/libft.h"
-#include "../Get_next_line/get_next_line.h"
+//#include "../Get_next_line/get_next_line.h"
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
