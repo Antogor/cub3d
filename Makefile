9e438ff318bbe08b9be856e2c31699889eeae18e
Antogor <antoniogarzoniniesta@gmail.com>
2020-01-26 17:12:17 +0100

    3d

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ NAME = cub3D
 
 FLAGS = gcc -Wall -Wextra -Werror
 
-MINILIBX = -L ./minilibx_mms_20191025_beta/ -lmlx -framework OpenGL -framework AppKit
+MINILIBX = -L ./minilibx_opengl_20191021/ -lmlx -framework OpenGL -framework AppKit
 
 FUN = cub3d.c ./GNL/get_next_line.c ./GNL/get_next_line_utils.c
 
@@ -10,7 +10,7 @@ OBJ = cub3d.o ./GNL/get_next_line.o ./GNL/get_next_line_utils.o
 
 all: $(NAME)
 
-$(NAME): $(OBJ) cub3d.h ./minilibx_mms_20191025_beta/mlx.h
+$(NAME): $(OBJ) cub3d.h ./minilibx_opengl_20191021/mlx.h
 	@$(FLAGS) $(MINILIBX) $(FUN)
 	@ar rc $(NAME) $(OBJ)
 	@ranlib $(NAME)
--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 12:24:33 by agarzon-          #+#    #+#             */
-/*   Updated: 2020/01/24 13:06:24 by agarzon-         ###   ########.fr       */
+/*   Updated: 2020/01/26 17:11:33 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,13 @@
 # include <stdio.h>
 # include <string.h>
 # include <math.h>
-# include "./minilibx_mms_20191025_beta/mlx.h"
+# include "./minilibx_opengl_20191021/mlx.h"
 
 typedef struct	s_lib_cub3d
 {
 	float		x;
 	float		y;
+	char		**map;
 }				t_vetor;
 
 typedef struct	s_lib_mlx
