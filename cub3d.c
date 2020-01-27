d08f37071c60fe5fde55d690fb9ebc92b008f3f8
Antonio Garzon iniesta <agarzon-@c2r7s1.42madrid.com>
2020-01-27 14:19:23 +0100

    Cub3d

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 12:24:14 by agarzon-          #+#    #+#             */
-/*   Updated: 2020/01/27 12:21:04 by agarzon-         ###   ########.fr       */
+/*   Updated: 2020/01/27 14:17:57 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,33 @@
 
 int		deal_key(int key, t_mlx *tab)
 {
-	if (key == 53)
-		mlx_string_put (tab->mlx_ptr, tab->window, 50, 300, 0x23BAC4, "CTHULHU");
-	else if (key == 0)
-		mlx_string_put (tab->mlx_ptr, tab->window, 100, 200, 0x23BAC4, "PRINTF");
-	else if (key == 13)
-		mlx_xpm_file_to_image (tab->mlx_ptr, "./cthulhu", (int *)50, (int *)50);
+	if (key == ESC)
+		mlx_destroy_window(tab->mlx_ptr, tab->window);
+	else if (key == A)
+		mlx_string_put (tab->mlx_ptr, tab->window, 4, 4, 0x23BAC4, "INSERTAR MAPA");
 //	ft_putnbr_fd(key, 1);
-//	param = 0;
 	return(0);
 }
-int		main()
+int		main(int argc, char **argv)
 {
 	t_mlx *tab;
-/*	int bpp;
-	int size_line;
-	int endian;*/
 
 	tab = (t_mlx *)malloc(sizeof(t_mlx *));
-	tab->mlx_ptr = mlx_init();
-	tab->window = mlx_new_window(tab->mlx_ptr, 1920, 1080, "cub3d");
-	
-	tab->image = mlx_new_image(tab->mlx_ptr, 5, 5);
-//	tab->image_data = mlx_get_data_addr(tab->image, &bpp, &size_line, &endian);
-/*	tab->image_data[40] = 0;
-	tab->image_data[41] = 255;
-	tab->image_data[42] = 200;*/
-//	mlx_put_image_to_window(tab->mlx_ptr, tab->window, tab->image, 0, 2);
-	mlx_key_hook(tab->window, deal_key, tab);
-	mlx_loop(tab->mlx_ptr);
+	if (argc == 2 || argc == 3)
+	{
+//		open(*argv, O_RDONLY);
+		tab->mlx_ptr = mlx_init();
+		tab->window = mlx_new_window(tab->mlx_ptr, 1920, 1080, "cub3d");
+		mlx_xpm_to_image (tab->mlx_ptr, argv, (int *)100, (int *)100);
+		tab->image = mlx_new_image(tab->mlx_ptr, 5, 5);
+		mlx_key_hook(tab->window, deal_key, tab);
+		mlx_loop(tab->mlx_ptr);
+	}
+	else
+	{
+		write(1, "Error\n", 6);
+	}
 	free(tab);
 	tab = NULL;
-
 	return (0);
 }
--- cub3d.h
+++ cub3d.h
@@ -6,12 +6,19 @@
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 12:24:33 by agarzon-          #+#    #+#             */
-/*   Updated: 2020/01/27 11:45:14 by agarzon-         ###   ########.fr       */
+/*   Updated: 2020/01/27 13:32:53 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _CUB3D_H
 # define _CUB3D_H
+# define A 0
+# define W 13
+# define S 1
+# define D 2
+# define LEFT 123
+# define RIGTH 124
+# define ESC 53
 
 # include "./libft/libft.h"
 # include "./GNL/get_next_line.h"
