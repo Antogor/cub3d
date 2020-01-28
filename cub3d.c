fd29d947795ba2f32c92d6fa05328bdf3c159188
Antonio Garzon iniesta <agarzon-@c2r7s5.42madrid.com>
2020-01-28 11:51:07 +0100

    Tests

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 12:24:14 by agarzon-          #+#    #+#             */
-/*   Updated: 2020/01/27 15:51:37 by agarzon-         ###   ########.fr       */
+/*   Updated: 2020/01/28 11:50:51 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int		deal_key(int key, t_mlx *tab)
 {
 	if (key == ESC)
-		mlx_destroy_window(tab->mlx_ptr, tab->window);
+		exit(ESC);
 	else if (key == A)
 		mlx_string_put (tab->mlx_ptr, tab->window, 4, 4, 0x23BAC4, "INSERTAR MAPA");
 //	ft_putnbr_fd(key, 1);
@@ -25,20 +25,19 @@ int		deal_key(int key, t_mlx *tab)
 int		main(int argc, char **argv)
 {
 	t_mlx *tab; 
-	int count_h;
-	int count_w;
-	int bpp;
-	int size_l;
-	int endian;
+	int fd;
+	char *a = NULL;
+	int bwr;
 
-	count_h = -1;	
 	tab = (t_mlx *)malloc(sizeof(t_mlx *));
-	if (argc == 2 || (argc == 3 && argv))
+	if (argc == 2 || argc == 3)
 	{
-//		open(*argv, O_RDONLY);
+		fd = open(argv[1], O_RDONLY);
+		bwr = read(fd, a, 100);
+		printf("%s", a);
 		tab->mlx_ptr = mlx_init();
 		tab->window = mlx_new_window(tab->mlx_ptr, X, Y, "cub3d");
-		tab->image = mlx_xpm_to_image (tab->mlx_ptr, argv, (int *)100, (int *)100);
+//		tab->image = mlx_xpm_to_image (tab->mlx_ptr, argv, (int *)100, (int *)100);
 //		tab->image = mlx_new_image (tab->mlx_ptr, 100, 100);
 //		tab->image_data = (int *)mlx_get_data_addr(tab->image, &bpp, &size_l, &endian);
 //		mlx_put_image_to_window(tab->mlx_ptr, tab->window, *argv, 200, 200);
