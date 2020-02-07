3045b40c8d8782666abf88211e06058aa4bafaae
Antonio Garzon iniesta <agarzon-@c1r14s6.42madrid.com>
2020-02-07 11:59:29 +0100

    arrayBI

--- test_print.c
+++ test_print.c
@@ -6,13 +6,14 @@
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/05 11:43:22 by agarzon-          #+#    #+#             */
-/*   Updated: 2020/02/05 17:26:10 by agarzon-         ###   ########.fr       */
+/*   Updated: 2020/02/07 11:58:44 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <mlx.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct  s_data {
     void        *img;
@@ -35,6 +36,22 @@ int		create_trgb(int t, int r, int g, int b)
 	return(t << 24 | r << 16 | g << 8 | b);
 }
 
+int **bi_array(int **array, int nrows, int ncolumns)
+{
+    int i;
+
+	i = 0;
+    if(!(array = malloc(nrows * sizeof(int *))))
+		return (NULL);
+    while (i < nrows)
+    {
+    	if(!(array[i] = malloc(ncolumns * sizeof(int))))
+			return (NULL);
+		i++;
+    }
+	return(array);
+}
+
 int main (void)
 {
 	void *mlx;
@@ -44,8 +61,12 @@ int main (void)
 	int colorB;
 	int colorT;
 	t_data img;
-	char    *dst;
-	
+	int    **dst;
+	int l = 0;
+	int q = 0;
+
+	dst = bi_array(dst, 1920, 1080);
+	while(q < 192)
 	mlx = mlx_init();
 	mlx_window = mlx_new_window(mlx, 1920, 1080, "test");
 	img.img = mlx_new_image(mlx, 1920, 1080);
@@ -54,9 +75,9 @@ int main (void)
 	colorG = create_trgb(0, 0, 255, 0);
 	colorB = create_trgb(0, 0, 0, 255);
 	colorT = create_trgb(255, 0, 0, 0);
-	dst = img.addr + (5 * img.line_length + 5 * (img.bits_per_pixel / 8));
-   *(unsigned int*)dst = colorR;
-	printf("%s", dst);
+	int     offset = (5 * img.line_length + 5 * (img.bits_per_pixel / 8));
+//	dst = offset;
+//	*(unsigned int*)dst = colorR;
 //	my_mlx_pixel_put(&img, 1000, 500, colorR);
 //	mlx_pixel_put(mlx, mlx_window, 1000, 500, colorR);
 /*	mlx_string_put(mlx, mlx_window, 6, 6, colorR, "ROJO");
