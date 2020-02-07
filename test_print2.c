e91b3eb43ca186e520b2462cc988ccbae974ff9e
Antonio Garzon iniesta <agarzon-@c1r14s6.42madrid.com>
2020-02-07 13:08:13 +0100

    pixel put

--- test_print2.c
+++ test_print2.c
@@ -1,19 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_print.c                                       :+:      :+:    :+:   */
+/*   test_print2.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/05 11:43:22 by agarzon-          #+#    #+#             */
-/*   Updated: 2020/02/07 13:04:55 by agarzon-         ###   ########.fr       */
+/*   Created: 2020/02/07 12:51:49 by agarzon-          #+#    #+#             */
+/*   Updated: 2020/02/07 12:52:35 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <mlx.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 typedef struct  s_data {
     void        *img;
@@ -21,7 +19,7 @@ typedef struct  s_data {
     int         bits_per_pixel;
     int         line_length;
     int         endian;
-} 				t_data;
+}               t_data;
 
 void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -31,69 +29,18 @@ void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
     *(unsigned int*)dst = color;
 }
 
-int		create_trgb(int t, int r, int g, int b)
-{
-	return(t << 24 | r << 16 | g << 8 | b);
-}
-
-int **bi_array(int **array, int nrows, int ncolumns)
-{
-    int i;
-
-	i = 0;
-    if(!(array = malloc(nrows * sizeof(int *))))
-		return (NULL);
-    while (i < nrows)
-    {
-    	if(!(array[i] = malloc(ncolumns * sizeof(int))))
-			return (NULL);
-		i++;
-    }
-	return(array);
-}
-
-int **fill_bi_array(int **array, int rows, int columns, int filled)
-{
-	int l;
-	int q;
-
-	l = 0;
-	q = 0;
-	while(l < rows)
-	{
-		q = 0;
-		while(q < columns)
-		{
-			array[l][q] = filled;
-			q++;
-		}
-		l++;
-	}
-	return (array);
-}
-
 int             main(void)
 {
     void    *mlx;
     void    *mlx_win;
     t_data  img;
-	int colorR;
-	int colorG;
-	int colorB;
-	int colorT;
 
-	
     mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1920, 1080, "cub3D");
+    mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
     img.img = mlx_new_image(mlx, 1920, 1080);
     img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
                                  &img.endian);
-	colorR = create_trgb(0, 255, 0, 0);
-	colorG = create_trgb(0, 0, 255, 0);
-	colorB = create_trgb(0, 0, 0, 255);
-	colorT = create_trgb(255, 0, 0, 0);
-	my_mlx_pixel_put(&img, 1000, 500, colorR);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-
+    my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
     mlx_loop(mlx);
 }
