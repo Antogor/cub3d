94369bdf82f11ffc64e3ac56c12519b4de1f8aff
Antogor <antoniogarzoniniesta@gmail.com>
2020-02-08 17:58:25 +0100

    try

--- test_print.c
+++ test_print.c
@@ -29,6 +29,8 @@ void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
     dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
     *(unsigned int*)dst = color;
+	*(unsigned int*)dst = color;
+	*(unsigned int*)dst = color;
 }
 
 int		create_trgb(int t, int r, int g, int b)
@@ -93,8 +95,8 @@ int             main(void)
 	colorG = create_trgb(0, 0, 255, 0);
 	colorB = create_trgb(0, 0, 0, 255);
 	colorT = create_trgb(255, 0, 0, 0);
-	my_mlx_pixel_put(&img, 260, 180, colorR);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	my_mlx_pixel_put(&img, 200, 100, colorR);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 200, 100);
 
     mlx_loop(mlx);
 }
