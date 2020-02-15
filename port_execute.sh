0da6830855b9a505afca37840e017584d89348d0
Antogor <antoniogarzoniniesta@gmail.com>
2020-02-15 16:02:29 +0100

    NS

--- port_execute.sh
+++ port_execute.sh
@@ -1,2 +1,2 @@
-gcc cub3d.c raycasting.c movement.c create_trgb.c paint.c -lmlx -lXext -lX11 -lm -o TEST3D_port && ./TEST3D_port | tee result2.txt
+gcc cub3d.c raycasting.c movement.c create_trgb.c paint.c -lmlx -lXext -lX11 -lm -o TEST3D_port && ./TEST3D_port | tee result3.txt
 #gcc test_print.c create_trgb.c paint.c -lmlx -lXext -lX11 -o TEST3D_port && ./TEST3D_port
